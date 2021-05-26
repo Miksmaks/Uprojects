f5001515f740e6f212566a37c808346c33222a24
Miksmaks <82056617+Miksmaks@users.noreply.github.com>
2021-05-26 13:06:58 +0300

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQL_PROJECT1.SQLConnections
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public int PostId { get;set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
